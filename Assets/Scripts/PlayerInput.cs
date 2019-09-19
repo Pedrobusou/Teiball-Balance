a4fc5277071e58835e5948585310784837d4d8c2
Pedro <pedrobusou@gmail.com>
2019-09-19 23:26:26 +0200

    Add player controls

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [Range(0, 300)]
+    public float rotationSpeed;
+
+    void Update()
+    {
+        //Keyboard
+        if (Input.anyKey) Move(Input.inputString);
+
+        //Joystick
+        transform.Rotate(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+    }
+
+    void Move(string key)
+    {
+        switch (key)
+        {
+            case "a": transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0); break;
+            case "w": transform.Rotate(0, rotationSpeed * Time.deltaTime, 0); break;
+            case "s": transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0); break;
+            case "d": transform.Rotate(-rotationSpeed * Time.deltaTime, 0, 0); break;
+        }
+    }
+}
