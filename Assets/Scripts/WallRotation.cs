6b98ad4014f4a623a70629bacc8e0fbb5cd02f21
Pedro <pedrobusou@gmail.com>
2019-09-19 19:32:09 +0200

    Add wall rotation

---
+++ Assets/Scripts/WallRotation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WallRotation : MonoBehaviour
+{
+    [Range(0, 300)]
+    public float speed = 5f;
+    public bool clockwise = true;
+
+    private void Start()
+    {
+        if (!clockwise) speed *= -1;
+    }
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
