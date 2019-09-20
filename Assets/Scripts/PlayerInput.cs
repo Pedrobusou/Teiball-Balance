4fce8ded46e66f0e14f3cbfae55d8cae3b27e864
Pedro <pedrobusou@gmail.com>
2019-09-20 16:15:32 +0200

    Limit board rotation

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,27 +1,55 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
     [Range(0, 300)]
     public float rotationSpeed;
 
+    private int maxDegrees = 15;
+    private int errorMargin = 5;
+    private float verticalMove;
+    private float horizontalMove;
+    private float verticalPos;
+    private float horizontalPos;
+
     void Update()
     {
-        //Keyboard
-        if (Input.anyKey) Move(Input.inputString);
+        verticalMove = Input.GetAxis("Vertical");
+        horizontalMove = Input.GetAxis("Horizontal");
 
-        //Joystick
-        transform.Rotate(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+        if (verticalMove != 0 || horizontalMove != 0) Move();
     }
 
-    void Move(string key)
+    void Move()
     {
-        switch (key)
+        if (verticalMove > 0 && Mathf.FloorToInt(transform.eulerAngles.x) >= maxDegrees && Mathf.FloorToInt(transform.eulerAngles.x) < maxDegrees + errorMargin)
         {
-            case "a": transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0); break;
-            case "w": transform.Rotate(0, rotationSpeed * Time.deltaTime, 0); break;
-            case "s": transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0); break;
-            case "d": transform.Rotate(-rotationSpeed * Time.deltaTime, 0, 0); break;
+            verticalMove = 0;
+            transform.eulerAngles = new Vector3(maxDegrees, transform.eulerAngles.y, transform.eulerAngles.z);
         }
+
+        if (horizontalMove < 0 && Mathf.FloorToInt(transform.eulerAngles.z) >= maxDegrees && Mathf.FloorToInt(transform.eulerAngles.z) < maxDegrees + errorMargin)
+        {
+            horizontalMove = 0;
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, maxDegrees);
+        }
+
+        if (verticalMove < 0 && Mathf.FloorToInt(transform.eulerAngles.x) <= 360 - maxDegrees && Mathf.FloorToInt(transform.eulerAngles.x) > 360 - maxDegrees - errorMargin)
+        {
+            verticalMove = 0;
+            transform.eulerAngles = new Vector3(360 - maxDegrees, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+
+        if (horizontalMove > 0 && Mathf.FloorToInt(transform.eulerAngles.z) <= 360 - maxDegrees && Mathf.FloorToInt(transform.eulerAngles.z) > 360 - maxDegrees - errorMargin)
+        {
+            horizontalMove = 0;
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 360 - maxDegrees);
+        }
+
+        horizontalPos += horizontalMove;
+        verticalPos += verticalMove;
+
+        transform.eulerAngles = new Vector3(verticalPos, 0, -horizontalPos);
     }
 }
